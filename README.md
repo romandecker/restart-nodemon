dc9c31679d4f81f0af22356026caaa7749f3f111
Roman Decker <Roman.Decker@zooplus.com>
2019-01-11 11:22:53 +0100

    Add basic functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+restart-nodemon
+===============
+
+Restart another nodemon process by touching a file. Also allows configuration via environment
+variables to easily use it in npm-scripts.
---
+++ src/restart-nodemon.ts
@@ -0,0 +1,27 @@
+import * as debug from 'debug';
+import * as touch from 'touch';
+import * as yargs from 'yargs';
+
+const log = debug('restart-nodemon');
+
+const argv = yargs.env('RESTART').option('touch', {
+    alias: 't',
+    describe:
+        'If present, trigger restart by touching the given file(s). Comma-separated list of file paths to touch.',
+    type: 'string',
+}).argv;
+
+(async function main() {
+    if (argv.touch) {
+        const filesToTouch = argv.touch.split(',');
+
+        for (const fileToTouch of filesToTouch) {
+            log('Touching', fileToTouch);
+            touch(fileToTouch);
+        }
+    }
+
+    if (!argv.touch) {
+        console.warn('Nothing to touch, not doing anything.');
+    }
+})();
